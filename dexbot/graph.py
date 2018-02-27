4c0817eb2c9f85c76c41ed7e9bfa44e12fe9da2d
Ian Haywood <ian@haywood.id.au>
2018-02-27 21:34:03 +1100

    use PNG bbackend, otherwise tries to load Tk and borks

--- dexbot/graph.py
+++ dexbot/graph.py
@@ -32,6 +32,8 @@ import numpy
 
 import datetime
 import time
+import matplotlib
+matplotlib.use('AGG') # anti-Grain Geometry: the backend for PNG files
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
